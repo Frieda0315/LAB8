ecf0dd0648b46965447860a68555e9860281270a
Frieda0315 <59812863+Frieda0315@users.noreply.github.com>
2021-03-22 15:09:03 -0600

    Update CityListTest.java

--- app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
+++ app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
@@ -27,9 +27,5 @@ class CityListTest {
 
         assertEquals(2, cityList.countCities());
         assertTrue(cityList.hasCity(new City("Regina", "Saskatchewan")));
-        
-        
     }
-    
-    
 }
